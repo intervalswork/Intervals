e1a3268dad861f6c866998e37b72f29e000df69b
intervalswork <intervals.work@gmail.com>
2026-08-17 03:06:07 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,7 @@
 if game.PlaceId == 662417684 then
-    -- ถ้าเรากำลังเล่นเกม Lucky Blocks ให้ดึงสคริปต์ Lucky Blocks มารัน
-    loadstring(game:HttpGet("เอาลิงก์ Raw ของไฟล์ luckyblocks.lua มาวางตรงนี้"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/intervalswork/Intervals/refs/heads/main/luckyblocksbattlegrounds.lua"))()
 
 elseif game.PlaceId == 2753915549 then
-    -- ถ้าเรากำลังเล่นเกม Blox Fruits ให้ดึงสคริปต์ Blox Fruits มารัน
     loadstring(game:HttpGet("เอาลิงก์ Raw ของไฟล์ bloxfruits.lua มาวางตรงนี้"))()
 
 end
