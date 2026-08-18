aa458bc1727e0a8a78877597ad4cec4c14a164c1
intervalswork <intervals.work@gmail.com>
2026-08-18 10:15:41 +0700

    Add support for new game PlaceId and script

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 662417684 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/intervalswork/Intervals/refs/heads/main/luckyblocksbattlegrounds.lua"))()
 
-elseif game.PlaceId == 2753915549 then
-    loadstring(game:HttpGet("เอาลิงก์ Raw ของไฟล์ bloxfruits.lua มาวางตรงนี้"))()
+elseif game.PlaceId == 78053126230950 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/intervalswork/Intervals/refs/heads/main/MakeSoccerPlayer.lua"))()
 
 end
